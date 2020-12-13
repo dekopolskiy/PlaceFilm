bc398b1b258953e2068b53d8ad35ccb5e8587baf
dmitri <dmitridecopolski@yandex.ru>
2020-12-13 14:07:37 +0300

    agian black

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,14 +3,16 @@ import Block_film from './Content/Block_film/Block_film'
 import Main_film from './Content/Main_film/Main_film'
 import styles from './Content.module.css'
 import Add_serial from './Content/Add_serial/Add_serial'
+import Greetings from './Content/Greetings/Greetings'
 
 
 const Section = (props) => {
     let mapPics = props.picsImg.map(item => <Block_film name={item.name} pics={item.img} />)
     return (
         <div className={styles.content}>
+            <Greetings />
             <Main_film />
-            <div className={styles.postFilm}>
+            <div className={styles.listSerials}>
                 {mapPics}
             </div>
             <Add_serial />
---
+++ src/components/Content/Content/Greetings/Greetings.jsx
@@ -0,0 +1,16 @@
+
+
+import styles from './Greetings.module.css'
+
+
+const Greetings = () => {
+    return (
+        <div className={styles.greetings}>
+            <h2>Themebau.</h2>
+            Themebau is a stylish universal template for all your projects
+            creative output. Minimalistic and easy to use, it has everything
+            you need to launch your website.
+        </div>
+    )
+}
+export default Greetings;
