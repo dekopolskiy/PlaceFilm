0b1d102df0fddd93247888b16503c24ddc13b482
dmitridecopolski <dmitridecopolski@yandex.ru>
2021-03-01 19:38:46 +0300

    test

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,11 +8,11 @@ const { default: content_reducer } = require("./content_reducer");
 
 
 let reducers = combineReducers({ 
-    contentRDC: content_reducer, //запускается store, запускается каждый reducer, возвращает state 
-    all_filmsRDC: all_films_reducer, //заполняет this._state 
-    actorsRDC: actors_reducer,//каждый редьюсер отвечает за отдельную часть state
-    accountRDC: account_reducer,
-    loginRDC: login_reducer,
+    contentRDC: content_reducer, //{mainPoster:, listSerials:, newPosterBody}//запускается store, запускается каждый reducer, возвращает state 
+    all_filmsRDC: all_films_reducer, //{all_films: []} //заполняет this._state 
+    actorsRDC: actors_reducer,//{items:,currentPage:,pageSize:,}//каждый редьюсер отвечает за отдельную часть state
+    accountRDC: account_reducer,//{acount: null}
+    loginRDC: login_reducer, //{data: {login:,id:,email:,}}
 })
 
 
